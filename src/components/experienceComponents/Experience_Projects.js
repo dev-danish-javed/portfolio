ae36532c45192431d81a56969c46a14db8ced535
Danish Javed <dev.danish.javed@gmail.com>
2024-06-22 13:28:38 +0530

    changes projects tab position to top

--- src/components/experienceComponents/Experience_Projects.js
+++ src/components/experienceComponents/Experience_Projects.js
@@ -60,12 +60,13 @@ const Experience_Projects = ({ activeRoute, handleMenuChange }) => {
   return (
     <div className="slide-in h-100">
       <div className="d-flex flex-column project-container h-100">
+        <div className="mt-4 mb-2 d-flex justify-content-around font-poppins border-accent rounded rounded-5  shadow-top-bottom-only  bg-light">
+          {projectsNavView}
+        </div>
         <div className="h-100">
           <Outlet />
         </div>
-        <div className="sticky-bottom d-flex justify-content-around font-poppins border-accent rounded rounded-5  shadow-top-bottom-only py-1 bg-light">
-          {projectsNavView}
-        </div>
+        
       </div>
     </div>
   );
